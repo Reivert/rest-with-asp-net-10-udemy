5ff6a476cc309be8eabfb8ec7f841dd0fa100a4b
Reivert <reivert.zulato@gmail.com>
2025-11-26 16:43:55 -0300

    Inclusão da pasta de sessão 8, implementação de log.

---
+++ 08_LogsInASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Model/Context/MSSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNET10Erudio.Model.Context
+{
+    public class MSSQLContext : DbContext
+    {
+        public MSSQLContext(DbContextOptions<MSSQLContext> options)
+            : base(options) { }
+        public DbSet<Persons> Persons { get; set; }
+    }
+}
---
+++ 08_LogsInASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Services/IPersonsServices.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET10Erudio.Model;
+
+namespace RestWithASPNET10Erudio.Services
+{
+    public interface IPersonsServices
+    {
+        Persons Create(Persons persons);
+        Persons Update(Persons persons);
+        void Delete(long id);
+        Persons FindById(long id);
+        List<Persons> FindAll();       
+    }
+}
