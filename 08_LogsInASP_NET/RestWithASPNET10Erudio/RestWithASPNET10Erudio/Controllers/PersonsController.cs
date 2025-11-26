8010a74b8fe1e67052777dd4725a1475b5bb86fd
Reivert <reivert.zulato@gmail.com>
2025-11-26 20:33:04 -0300

    Inclusão de logs, biblioteca serialog e inclusão de novo controller para testes de logs.

--- 08_LogsInASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Controllers/PersonsController.cs
+++ 08_LogsInASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Controllers/PersonsController.cs
@@ -9,23 +9,30 @@ namespace RestWithASPNET10Erudio.Controllers
     public class PersonsController : ControllerBase
     {
         private IPersonsServices _personsService;
+        private  readonly ILogger<PersonsController> _logger;
 
-        public PersonsController(IPersonsServices personsService)
+        public PersonsController(IPersonsServices personsService, 
+            ILogger<PersonsController> logger)
         {
             _personsService = personsService;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation("Getting all persons");
             return Ok(_personsService.FindAll());
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(long id) {
+            _logger.LogInformation("Getting person with id {Id}", id);
+
             var person = _personsService.FindById(id);
             if (person == null)
             {
+                _logger.LogWarning("Person with id {Id} not found", id);
                 return NotFound();
             }
             return Ok(person);
@@ -34,9 +41,12 @@ namespace RestWithASPNET10Erudio.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Model.Persons persons)
         {
+            _logger.LogInformation("Creating a new person {first_name}", persons.FirstName);
+
             var createdPerson = _personsService.Create(persons);
             if (createdPerson == null)
             {
+                _logger.LogError("Error creating person {first_name}", persons.FirstName);
                 return BadRequest();
             }
             return Ok(createdPerson);
@@ -45,18 +55,26 @@ namespace RestWithASPNET10Erudio.Controllers
         [HttpPut]
         public IActionResult Put([FromBody] Model.Persons persons)
         {
+            _logger.LogInformation("Updating person with id {Id}", persons.Id);
+
             var updatedPerson = _personsService.Update(persons);
             if (updatedPerson == null)
             {
+                _logger.LogError("Error updating person with id {Id}", persons.Id);
                 return BadRequest();
             }
+
+            _logger.LogDebug("Person with id {Id} updated successfully", persons.Id);
             return Ok(updatedPerson);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            _logger.LogInformation("Deleting person with id {Id}", id);
+
             _personsService.Delete(id);
+            _logger.LogDebug("Person with id {Id} deleted successfully", id);
             return NoContent();
         }
 
